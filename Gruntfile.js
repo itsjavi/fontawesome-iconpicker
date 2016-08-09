456d3643464ce4e50209e3ec51b11783a5eb2eb2
Alex Schmoe <aschmoe@gmail.com>
2016-08-08 17:06:45 -0700

    updating how uglify compress: true is called, https://github.com/gruntjs/grunt-contrib-uglify/issues/298

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function(grunt) {
         uglify: {
             distMin: {
                 options: {
-                    compress: true,
+                    compress: {},
                     beautify: false
                 },
                 files: {
